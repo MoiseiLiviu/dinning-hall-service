4be2ebe33fb6f10c6ae71ba0ccf1b0d212ca23dc
Moisei Liviu <liviu.moisei@amdaris.com>
2022-10-12 10:48:30 +0300

    Logging the sent orders.

--- src/main/java/com/restaurantapp/dinninghallservice/constants/enums/KitchenUrls.java
+++ src/main/java/com/restaurantapp/dinninghallservice/constants/enums/KitchenUrls.java
@@ -2,5 +2,5 @@ package com.restaurantapp.dinninghallservice.constants.enums;
 
 public interface KitchenUrls {
 
-    String KITCHEN_SERVICE_URL = "http://localhost:8083/kitchen/order";
+    String KITCHEN_SERVICE_URL = "http://kitchen-service:8083/kitchen/order";
 }
